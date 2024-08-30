8ae49960e1cdefbffc4edb3270ca51d1248cbb28
Kenzo Mailleux <kmailleu@fu-c4-p3.s19.be>
2024-08-30 17:00:12 +0200

    srcs directory added

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmailleu <kmailleu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/24 17:40:55 by kenzo             #+#    #+#             */
-/*   Updated: 2024/08/30 16:58:01 by kmailleu         ###   ########.fr       */
+/*   Created: 2024/08/30 16:59:50 by kmailleu          #+#    #+#             */
+/*   Updated: 2024/08/30 16:59:51 by kmailleu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef H_MINISHELL_H
-# define H_MINISHELL_H
-
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
-typedef struct s_tokens
-{
-	char	*str;
-	int		
-}				t_tokens;
-
-typedef struct s_minishell
-{
-	int		end;
-}				t_minishell;
-
-
-
-
-#endif
