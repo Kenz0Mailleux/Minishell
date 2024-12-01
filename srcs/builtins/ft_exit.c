ea268d4a082cb28d184229d37aaaca81a93f7406
kmailleu <kenzo.mailleux@gmail.com>
2024-12-01 15:59:58 +0100

    fini

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,18 +6,33 @@
 /*   By: kenzo <kenzo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:21:43 by kenzo             #+#    #+#             */
-/*   Updated: 2024/11/30 18:23:04 by kenzo            ###   ########.fr       */
+/*   Updated: 2024/12/01 15:59:25 by kenzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+#include "../../inc/minishell.h"
+
+static int	exit_arg(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if (arg[i] == '+' || arg[i] == '-')
+		i++;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	ft_exit(t_data *data, char **args)
 {
 	int	exit_code;
-	int	i;
 
-	i = -1;
 	if (args[1])
 	{
 		if (args[2])
@@ -25,18 +40,20 @@ void	ft_exit(t_data *data, char **args)
 			ft_printf("minishell: exit: too many arguments\n");
 			return ;
 		}
-		while (args[1][++i])
+		if (!exit_arg(args[1]))
 		{
-			if (!ft_isdigit(args[1][i]) && !(i == 0 && (args[1][i] == '+')))
-			{
-				ft_printf("minishell: exit: %s:", args[1]);
-				ft_printf(" positive numeric argument required\n");
-				return ;
-			}
+			ft_printf("minishell: exit: %s: ", args[1]);
+			ft_printf("numeric argument required\n");
+			return ;
 		}
-		exit_code = ft_atoi(args[1]) % 256;
+		exit_code = ft_atoi(args[1]);
+		if (exit_code < 0)
+			exit_code = 256 + (exit_code % 256);
+		else
+			exit_code %= 256;
 	}
 	else
 		exit_code = 0;
+
 	free_all(data, exit_code);
 }
